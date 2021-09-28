cd96e85cadc5884d6487b5be15f3d130b6c4ce06
Jkawala <91265409+Jkawala@users.noreply.github.com>
2021-09-28 03:45:07 +0100

    Add files via upload

---
+++ name.py
@@ -0,0 +1,17 @@
+print("This is my first program")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+character_name = "Jake"
+character_age = "23"
+
+print("My name is " + character_name + ",")
+print("I am " + character_age + " years old")
+
+character_name = "Jane"
+character_age = "29"
+is_Female = False
+print("He really liked" + character_name)
+print("she was " + character_age + " years old")
