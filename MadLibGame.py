7fe361f2f146c520b70e0e00e6f162f3785e7e44
Jkawala <91265409+Jkawala@users.noreply.github.com>
2021-09-28 05:23:10 +0100

    Add files via upload

---
+++ MadLibGame.py
@@ -0,0 +1,25 @@
+# An example of a mad libs game on python #
+
+adjective = input("Enter a adjective: ")
+name = input("Enter a name: ")
+noun = input("Enter a noun")
+exclamation = input("Enter an exclamation")
+verb = input("Enter a verb")
+adverb = input("Enter an adverb")
+adjective_two = input("Enter the second adjective")
+noun_two = input("Enter the second noun")
+noun_three = input("Enter the third noun")
+noun_four = input("Enter the fourth noun")
+noun_five = input("Enter a five noun")
+adjective_three = input("Enter a third adjective")
+adjective_four = input("Enter a fourth adjective")
+
+print("Today I bought a " + adjective)
+print("pumpkin to make a " + name + " O Lantern for Halloween")
+print("I take out the carving " + noun + " and stick it through the top of the pumpkin")
+print(exclamation + " there is" + adjective_two + noun_two + " coming out of it")
+print("It is " + verb + " all over the pumpkin")
+print("I took the top off the pumpkin but there were no" + noun_three + " , only " + noun_four + " inside.")
+print("Despite all of this, I continue to " + adverb + " carve a picture of a " + noun_five + "on the pumpkin which "
+                                                                                              "looks just perfect!")
+print("Even though it is a" + adjective_three + " pump kin, it is still a" + adjective_four + "pumpkin for Halloween!")
