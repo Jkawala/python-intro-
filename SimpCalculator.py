0457dba92b5b4889044985f18c1cf6660a67a69b
Jkawala <91265409+Jkawala@users.noreply.github.com>
2021-09-28 04:07:19 +0100

    Add files via upload

---
+++ SimpCalculator.py
@@ -0,0 +1,15 @@
+phrase = "Running Code"
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.upper().isupper())
+print(len(phrase))
+
+phrase_two = "Not running code"
+print(phrase.lower().islower())
+print(phrase_two[0])
+
+num1 = input("Enter a random number: ")
+num2 = input("enter another number: ")
+result = float(num1) + float(num2)
+
+print(result)
